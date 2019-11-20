eb46cc420ea3abb617fa31f9499b516c7e623fa3
Osama Mohammed Najjar <osama_5052@hotmail.com>
2019-11-20 15:04:13 +0300

    add router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MapsComponent } from './maps/maps.component';
+import { BooksComponent } from './books/books.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { RemoteComponent } from './maps/remote/remote.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'maps', component: MapsComponent, children: [
+    {path: 'remote', component: RemoteComponent}
+  ]},
+  {path: 'books', component: BooksComponent},
+  {path: '**', component: NotFoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MapsComponent } from './maps/maps.component';
+import { BooksComponent } from './books/books.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { RemoteComponent } from './maps/remote/remote.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MapsComponent,
+    BooksComponent,
+    NotFoundComponent,
+    RemoteComponent
   ],
   imports: [
     BrowserModule,
